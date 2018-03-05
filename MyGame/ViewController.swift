ce6f31827cc401f56ad1b2e76422734e0af887ba
上田祐也 <yu@apple-no-macbook-pro.local>
2018-03-05 21:44:00 +0900

    Initial Commit

---
+++ MyGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGame
+//
+//  Created by apple on 2018/03/05.
+//  Copyright © 2018年 Dotinstall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
